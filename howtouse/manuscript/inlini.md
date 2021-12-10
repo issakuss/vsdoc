e8ce146d33e71f102fd5cb27f87a8e8f54701bb3
issakuss <issakuss@gmail.com>
2021-12-10 06:39:59 +0000

    🔬 feat: inlini have new function
    To execute code before embed

--- howtouse/manuscript/inlini.md
+++ howtouse/manuscript/inlini.md
@@ -1,8 +1,14 @@
 # How to use inlini
 The inline allows you embed variables in your manuscript.
-
+You can convert value ("var") with python code written after "!!".
+Though you can't include "{" and "}", you can use some functions pval() and dround().
+Don't use space also.
 
 ## Example
 We used the %{method:stats.test}.
 %{result:subject.n} participated the experiment.
 The significant result was found (t = %{result:ttest.t}; p = %{result:ttest.p}).
+
+The correlation in condition A was siginificant (p %{result:pearson.p1!!pval(var,0.001)})
+The correlation in condition B was siginificant (p %{result:pearson.p2!!dround(var,2)})
+The correlation in condition B was siginificant (p %{result:pearson.p2!!pval(dround(var,2),0.001)})
--- howtouse/src/filters/inlini.py
+++ howtouse/src/filters/inlini.py
@@ -5,36 +5,55 @@ from pathlib import Path
 from pandocfilters import toJSONFilter, Str
 
 
-# Input relative paths from the working directory
-INI_PATHS = ['method.ini', 'report/result.ini']
+dire = Path(__file__).resolve().parent
+pattern = re.compile('%\{(.*)\}')
+parser_posit = ConfigParser()
+parser_posit.read(dire / 'iniposit.ini')
+
+posits = dict(parser_posit.items('posit'))
+parsers = dict()
+for key, posit in posits.items():
+    parser = ConfigParser()
+    parser.read(posit)
+    parsers[key] = parser
 
 
-def inline(key, value, format_, meta):
-    ###
-    if value is not None:
-        if key=='Cite':
-            with open('/mnt/vsdoc/debug.text', 'a') as f:
-                f.write(str(value[0]) + '/')
-                f.write('#####\n')
-    ###
+def pval(var, mini=-float('inf')):
+    if float(var) < mini:
+        return '< .001'
+    else:
+        return '= ' + var[1:]
 
+def dround(var, d):
+    return str(round(float(var), 2))
+
+
+def inline(key, value, *_):
     if key != 'Str':
         return
-    m = re.compile('%\{(.*)\}').match(value)
+    m = pattern.match(value)
     if m is None:
         return
-    field = re.split('[:.]', m.group(1))
+
+    field = re.split('!!', m.group(1))
+    if len(field) == 2:
+        field, code = field
+    else:
+        field = field[0]
+        code = None
+    field = re.split('[:.]', field)
+
     if len(field) != 3:
         return
+
     var = dict(parsers[field[0]].items(field[1]))[field[2]]
+    if code:
+        var = eval(code)
+
     return Str(value[:m.span()[0]] + var + value[m.span()[1]:]) 
 
 
 if __name__ == '__main__':
-    parsers = dict()
-    for path in INI_PATHS:
-        parser = ConfigParser()
-        parser.read(path)
-        parsers[Path(path).stem] = parser
-
     toJSONFilter(inline)
+    if False: # Test code
+        replaced = inline('Str', '%{method:stats.test!!pval(var)}', None, None)
